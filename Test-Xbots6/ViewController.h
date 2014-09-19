6146b94e4e6eea6cf37a4bf7b5b279928dd94d60
nick-fueled <nick@fueled.com>
2014-09-18 21:23:27 -0400

    Initial Commit

---
+++ Test-Xbots6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-Xbots6
+//
+//  Created by Nick McConnell on 9/18/14.
+//  Copyright (c) 2014 Nick McConnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test-Xbots6/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test-Xbots6
+//
+//  Created by Nick McConnell on 9/18/14.
+//  Copyright (c) 2014 Nick McConnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
